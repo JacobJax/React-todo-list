2c1093eac943a9aa3c9da027cbb2f6233c76eb9f
Jacob Odero <jackjax617@gmail.com>
2021-02-19 19:56:09 +0300

    add delete button

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -1,14 +1,19 @@
 import './Todoitem.css'
 
-const Todoitem = ({id, name, completed, handleDblClick}) => {
+const Todoitem = ({id, name, completed, handleDblClick, deleteTask}) => {
 
     const handleClick = () => {
         handleDblClick(id)
     }
 
+    const handleDelete = () => {
+        deleteTask(id)
+    }
+
     return (
         <div className="todoitem" style={completed ? { borderLeft: '5px #000 solid' } : { borderLeft: 'none' }} onDoubleClick={handleClick}>
-            <p style={completed ? { textDecoration: 'line-through' } : { textDecoration: 'none' }}>{name}</p>
+            <p style={completed ? { textDecoration: 'line-through' } : { textDecoration: 'none' }} disabled={completed}>{name}</p>
+            <button onClick={handleDelete}>Delete</button>
         </div>
     );
 }
